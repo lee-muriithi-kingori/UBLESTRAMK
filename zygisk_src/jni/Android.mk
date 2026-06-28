461bd26d27a7a4590bb3857137ba0c90015d605f
!-Hen <leekingori54@gmail.com>
2026-06-28 16:11:50 +0300

    Add Android.mk build config

---
+++ zygisk_src/jni/Android.mk
@@ -0,0 +1,15 @@
+# UBLESTRAMK Zygisk Module - Android Makefile
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := ublestramk
+LOCAL_SRC_FILES := main.cpp
+LOCAL_CFLAGS    := -O2 -fvisibility=hidden -fvisibility-inlines-hidden \
+                   -fno-exceptions -fno-rtti -fno-stack-protector \
+                   -DANDROID -DNDEBUG
+LOCAL_CPPFLAGS  := -std=c++17
+LOCAL_LDLIBS    := -llog
+LOCAL_LDFLAGS   := -Wl,--exclude-libs,ALL
+
+# Build as shared library for Zygisk
+include $(BUILD_SHARED_LIBRARY)
